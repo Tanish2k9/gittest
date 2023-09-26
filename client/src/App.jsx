3d9dacef82bc8b763927dda4017424cd2302b92f
tanish2k9 <tanishchauhanmin@gmail.com>
2023-09-26 19:42:25 +0530

    create header component and replace react-router-dom from root to  client folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import {Routes,Route} from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
