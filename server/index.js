5d05748e537131eed86f5f263db1ed9d7dda9df5
tanish2k9 <tanishchauhanmin@gmail.com>
2023-09-27 12:52:44 +0530

    create signup api route

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import Connection from "./connect.js";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 
 Connection();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.use('/api/users',userRoutes);
+app.use('/api/auth',authRoutes);
 
 app.listen(3000,()=>{
     console.log('app listening on port 3000')
