8fdc86bde8df18835d9b649a36a373bd144b4678
tanish2k9 <tanishchauhanmin@gmail.com>
2023-09-27 12:21:02 +0530

    cretae test route

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import Connection from "./connect.js";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 const app = express();
 
 Connection();
 
-app.listen(4000,()=>{
+app.use('/api/users',userRoutes);
+
+app.listen(3000,()=>{
     console.log('app listening on port 3000')
 })
